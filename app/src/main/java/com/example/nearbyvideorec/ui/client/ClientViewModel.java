04f5c5135e53be186c9166a58d99f7f5d462031d
Razac-elda <30749146+leomazzon@users.noreply.github.com>
2020-11-02 17:05:29 +0100

    Refactor menu items.

--- app/src/main/java/com/example/nearbyvideorec/ui/client/ClientViewModel.java
+++ app/src/main/java/com/example/nearbyvideorec/ui/client/ClientViewModel.java
@@ -1,16 +1,16 @@
-package com.example.nearbyvideorec.ui.home;
+package com.example.nearbyvideorec.ui.client;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class HomeViewModel extends ViewModel {
+public class ClientViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public HomeViewModel() {
+    public ClientViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("This is client fragment");
     }
 
     public LiveData<String> getText() {
