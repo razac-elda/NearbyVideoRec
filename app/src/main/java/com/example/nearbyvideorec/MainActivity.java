af16c8507e50e9943caeb540572c7f0a9ae75bab
Razac-elda <30749146+leomazzon@users.noreply.github.com>
2020-12-01 19:22:06 +0100

    Comments and formatting

--- app/src/main/java/com/example/nearbyvideorec/MainActivity.java
+++ app/src/main/java/com/example/nearbyvideorec/MainActivity.java
@@ -55,18 +55,22 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // Singleton created for the first and only time.
         savedUIData = SavedUIData.INSTANCE;
         BottomNavigationView navView = findViewById(R.id.nav_view);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_client, R.id.navigation_server, R.id.navigation_video)
                 .build();
+
         // Later we use the navController to refresh the fragment
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
         context = getApplicationContext();
         activity_context = MainActivity.this;
         SERVICE_ID = getPackageName();
@@ -78,11 +82,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void requestConnect(String caller) {
+
         if (caller.equals("CLIENT")) {
+
             if (!savedUIData.getServer_status_switch()) {
                 deviceRole = "Client";
                 startDiscovery();
             } else {
+
                 new AlertDialog.Builder(activity_context, R.style.Theme_ConnectionDialog)
                         .setTitle(getString(R.string.error))
                         .setMessage(getString(R.string.error_discovery_server))
@@ -92,15 +99,27 @@ public class MainActivity extends AppCompatActivity {
                 savedUIData.setClient_status_switch(false);
             }
             navController.navigate(R.id.navigation_client);
+
         } else {
+
             // Caller is SERVER
             if (!savedUIData.getClient_status_switch()) {
                 deviceRole = "Server";
                 startAdvertising();
             } else {
+                // Caller is client and want to become a Server
                 String endpointId = null;
+
+                // Get Server ID, only one entry on connectedEndpoints when acting as Client
                 for (String endpoint : connectedEndpoints.keySet())
                     endpointId = endpoint;
+
+                /*
+                 * TODO:Only working with two devices, need to update for multi device.
+                 * Idea:Send to other devices a request to switch off-on discovery without
+                 * disconnecting. Maybe send new Server endpointId and connect with requestConnection
+                 * Unknown:Connection needs to authenticate again?
+                 */
                 sendMessage(endpointId, "Change");
                 startAdvertising();
                 Nearby.getConnectionsClient(context).stopDiscovery();
@@ -112,17 +131,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void requestDisconnect(String caller) {
+
         if (caller.equals("CLIENT")) {
+
             Nearby.getConnectionsClient(context).stopDiscovery();
             savedUIData.setClient_status_switch(false);
             navController.navigate(R.id.navigation_client);
         } else {
+
             // Caller is SERVER
             Nearby.getConnectionsClient(context).stopAdvertising();
             savedUIData.setServer_status_switch(false);
             navController.navigate(R.id.navigation_server);
         }
+
+        // Disconnects from, and removes all traces of, all connected and/or discovered endpoints
         Nearby.getConnectionsClient(context).stopAllEndpoints();
+        // Clear connected endpoints
         connectedEndpoints.clear();
     }
 
@@ -131,19 +156,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startAdvertising() {
+
         AdvertisingOptions advertisingOptions =
                 new AdvertisingOptions.Builder().setStrategy(STRATEGY).build();
+
         Nearby.getConnectionsClient(context)
                 .startAdvertising(
                         getUserNickname(), SERVICE_ID, connectionLifecycleCallback, advertisingOptions)
                 .addOnSuccessListener(
                         (Void unused) -> {
-                            // TODO:We're advertising!
+                            // We're advertising!
                             Toast.makeText(activity_context, getString(R.string.advertising), Toast.LENGTH_SHORT).show();
                         })
                 .addOnFailureListener(
                         (Exception e) -> {
-                            // TODO:We were unable to start advertising.
+                            // We were unable to start advertising.
                         });
     }
 
@@ -175,39 +202,53 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onConnectionResult(@NotNull String endpointId, ConnectionResolution result) {
+
                     switch (result.getStatus().getStatusCode()) {
+
                         case ConnectionsStatusCodes.STATUS_OK:
-                            // TODO:We're connected! Can now start sending and receiving data.
+                            // We're connected! Can now start sending and receiving data.
+                            // Save new connected endpoint
                             connectedEndpoints.put(endpointId, temp_connectionInfo);
+                            // Refresh fragment
                             if (deviceRole.equals("Client"))
                                 navController.navigate(R.id.navigation_client);
                             else
                                 navController.navigate(R.id.navigation_server);
+
                             Toast.makeText(activity_context, getString(R.string.connected_to) +
                                     " " + temp_connectionInfo.getEndpointName(), Toast.LENGTH_LONG).show();
                             break;
+
                         case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
-                            // TODO:The connection was rejected by one or both sides.
+                            // The connection was rejected by one or both sides.
+
                             Toast.makeText(activity_context, getString(R.string.connection_rejected), Toast.LENGTH_SHORT).show();
                             break;
+
                         case ConnectionsStatusCodes.STATUS_ERROR:
-                            // TODO:The connection broke before it was able to be accepted.
+                            // The connection broke before it was able to be accepted.
+
                             Toast.makeText(activity_context, getString(R.string.connection_error), Toast.LENGTH_SHORT).show();
                             break;
+
                         default:
-                            // TODO:Unknown status code
+                            // Unknown status code
                     }
                 }
 
                 @Override
                 public void onDisconnected(@NotNull String endpointId) {
-                    /* TODO:We've been disconnected from this endpoint. No more data can be
+                    /* We've been disconnected from this endpoint. No more data can be
                         sent or received. */
+                    // Retrieve old endpoint info
                     ConnectionInfo endpointInfo = connectedEndpoints.get(endpointId);
+
                     if (endpointInfo != null) {
                         Toast.makeText(activity_context, getString(R.string.device_disconnected)
                                 + " " + endpointInfo.getEndpointName(), Toast.LENGTH_LONG).show();
+                        // Remove old endpoint
                         connectedEndpoints.remove(endpointId);
+                        // Refresh fragments
                         if (deviceRole.equals("Client"))
                             navController.navigate(R.id.navigation_client);
                         else
@@ -218,25 +259,41 @@ public class MainActivity extends AppCompatActivity {
             };
 
     public void sendMessage(String endpointId, String msg) {
+
         if (endpointId != null) {
+            // Convert the message to Bytes
             byte[] bytes = msg.getBytes(StandardCharsets.UTF_8);
+            // Generate payload and send it to destination
             Payload bytesPayload = Payload.fromBytes(bytes);
             Nearby.getConnectionsClient(context).sendPayload(endpointId, bytesPayload);
         }
     }
 
     private PayloadCallback payloadCallback = new PayloadCallback() {
+
         @Override
         public void onPayloadReceived(@NonNull String endpointId, @NonNull Payload payload) {
+
             if (payload.getType() == Payload.Type.BYTES) {
+                // Convert the payload from Bytes to a String
                 String msg = new String(payload.asBytes(), StandardCharsets.UTF_8);
                 switch (msg) {
+
                     case "Change":
+                        // Request to change Server
+                        /*
+                         * TODO:Only working with two devices, need to update for multi device.
+                         * Idea:Send to other devices a request to switch off-on discovery without
+                         * disconnecting. Maybe send new Server endpointId and connect with requestConnection
+                         * Unknown:Connection needs to authenticate again?
+                        */
                         startDiscovery();
                         Nearby.getConnectionsClient(context).stopAdvertising();
                         savedUIData.setClient_status_switch(true);
                         savedUIData.setServer_status_switch(false);
                         navController.navigate(R.id.navigation_client);
+                        break;
+
                     default:
                         Toast.makeText(activity_context, msg, Toast.LENGTH_LONG).show();
                         break;
@@ -246,47 +303,51 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onPayloadTransferUpdate(@NonNull String endpointId, @NonNull PayloadTransferUpdate payloadTransferUpdate) {
-
+            // Used with files payload to keep tracking of the transfer
         }
     };
 
     private void startDiscovery() {
+
         DiscoveryOptions discoveryOptions =
                 new DiscoveryOptions.Builder().setStrategy(STRATEGY).build();
+
         Nearby.getConnectionsClient(context)
                 .startDiscovery(SERVICE_ID, endpointDiscoveryCallback, discoveryOptions)
                 .addOnSuccessListener(
                         (Void unused) -> {
-                            // TODO:We're discovering!
+                            // We're discovering!
                             Toast.makeText(activity_context, getString(R.string.discovering), Toast.LENGTH_SHORT).show();
                         })
                 .addOnFailureListener(
                         (Exception e) -> {
-                            // TODO:We're unable to start discovering.
+                            // We're unable to start discovering.
                         });
     }
 
     private final EndpointDiscoveryCallback endpointDiscoveryCallback =
             new EndpointDiscoveryCallback() {
+
                 @Override
                 public void onEndpointFound(@NotNull String endpointId, @NotNull DiscoveredEndpointInfo info) {
+
                     // An endpoint was found. We request a connection to it.
                     Nearby.getConnectionsClient(context)
                             .requestConnection(getUserNickname(), endpointId, connectionLifecycleCallback)
                             .addOnSuccessListener(
                                     (Void unused) -> {
-                                        /* TODO:We successfully requested a connection. Now both sides
+                                        /* We successfully requested a connection. Now both sides
                                             must accept before the connection is established. */
                                     })
                             .addOnFailureListener(
                                     (Exception e) -> {
-                                        // TODO:Nearby Connections failed to request the connection.
+                                        // Nearby Connections failed to request the connection.
                                     });
                 }
 
                 @Override
                 public void onEndpointLost(@NotNull String endpointId) {
-                    // TODO:A previously discovered endpoint has gone away.
+                    // A previously discovered endpoint has gone away.
                 }
             };
 
